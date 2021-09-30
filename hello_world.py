30d069c946e86a68a1f316707e22759ee5cf0655
c1027525 <c1027525@newcastle.ac.uk>
2021-09-30 15:13:54 +0100

    Added background scenery

--- hello_world.py
+++ hello_world.py
@@ -1,10 +1,19 @@
-import sys
-import platform
+from direct.showbase.ShowBase import ShowBase
 
-print("hello")
+class MyApp(ShowBase):
+
+    def __init__(self):
+        ShowBase.__init__(self)
+
+        self.scene = self.loader.loadModel("models/environment")
+
+        self.scene.reparentTo(self.render)
+
+        self.scene.setScale(0.25, 0.25, 0.25)
+        self.scene.setPos(-8, 42, 0)
+
+app = MyApp()
+app.run
 
 
-print(1, sys.version)
-print(2, platform.python_implementation())
-print(3, sys.executable)
 
