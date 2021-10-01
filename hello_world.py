11c6b665af41fa424c0334bfdf1233851dc1e8a0
c1027525Newcastle <c1027525@newcastle.ac.uk>
2021-10-01 13:53:55 +0100

    Added the 3D Panda

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,8 @@
+from math import pi, sin, cos
+
 from direct.showbase.ShowBase import ShowBase
+from direc.task import Task
+from direct.actor.Actor import Actor
 
 class MyApp(ShowBase):
 
@@ -11,6 +15,20 @@ class MyApp(ShowBase):
 
         self.scene.setScale(0.25, 0.25, 0.25)
         self.scene.setPos(-8, 42, 0)
+
+        self.pandaActor = Actor("models/panda-model",
+                                {"walk": "models/panda-walk4"})
+        self.pandaActor.setScale(0.005, 0.005, 0.005)
+        self.pandaActor.reparentTo(self.render)
+
+        self.pandaActor.loop("walk")
+
+    def spinCameraTask(self, task):
+        angleDegrees = task.time * 6.0
+        angleRadians = angleDegrees * (pi/ 180.0)
+        self.camera.setPos(20* sin(angleRadians), -20.0 * cos(angleRadians), 3)
+        self.camera.setHpr(angleDegrees, 0, 0)
+        return Task.cont
 
 app = MyApp()
 app.run
